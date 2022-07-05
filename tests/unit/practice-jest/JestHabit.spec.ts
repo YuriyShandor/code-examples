e4646deb347ecbec872337a1ee1ca386b730373d
Yuriy Shandor <yuriysutosoft@gmail.com>
2022-07-05 16:58:38 +0300

    add jest testing examples

--- tests/unit/practice-jest/JestHabit.spec.ts
+++ tests/unit/practice-jest/JestHabit.spec.ts
@@ -2,34 +2,33 @@ import { shallowMount } from '@vue/test-utils';
 import JestHabit from '@/components/tests/unit/JestHabit.vue';
 
 describe('JestHabit.vue', () => {
-  it('Makes sure the habit name is rendered', () => {
-    const habitName = 'Learn something new';
-    const wrapper = shallowMount(JestHabit, {
+  let wrapper = null;
+  const habitName = 'Learn something new';
+
+  beforeEach(() => {
+    wrapper = shallowMount(JestHabit, {
       propsData: {
         name: habitName,
       },
     });
+  })
+
+  afterEach(() => {
+    wrapper = null;
+  })
+
+  it('Makes sure the habit name is rendered', () => {
     expect(wrapper.props().name).toBe(habitName);
     expect(wrapper.text()).toContain(habitName);
   });
 
   it('Marks the habit as completed', async () => {
-    const wrapper = shallowMount(JestHabit, {
-      propsData: {
-        name: 'Learn something new',
-      },
-    });
     const box = wrapper.find('.habit__box');
     await box.trigger('click');
     expect(box.text()).toContain("✔");
   });
 
   it('Calls the onHabitDone method', async () => {
-    const wrapper: any = shallowMount(JestHabit, {
-      propsData: {
-        name: 'Learn something new',
-      },
-    });
     const spyOnHabitDone = jest.spyOn(wrapper.vm, 'onHabitDone');
     const box = wrapper.find('.habit__box');
     await box.trigger('click');
@@ -37,11 +36,6 @@ describe('JestHabit.vue', () => {
   });
 
   it('Updates the habit props name', async () => {
-    const wrapper = shallowMount(JestHabit, {
-      propsData: {
-        name: 'Learn something new',
-      },
-    });
     const newHabitName = 'Brush my teeth';
     await wrapper.setProps({
       name: newHabitName,
