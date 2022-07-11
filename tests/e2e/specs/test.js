ea38ca68614e01d6a44ff021db81c5cee7d3a734
Yuriy Shandor <yuriysutosoft@gmail.com>
2022-07-11 19:56:33 +0300

    add e2e testing via cypress

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,14 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
+describe('Tests for Home Page', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
+    cy.contains('.banner-ttl', 'Code Examples');
+    cy.pause();
+    cy.get('.global-menu-block').should('not.be.visible');
+    cy.get('.global-menu-btn').should('exist');
+    cy.get('.global-menu-btn').click();
+    cy.get('.global-menu-block').should('be.visible');
+    cy.get('.global-menu-btn').invoke('attr', 'class').should('contain', 'active');
   });
 });
