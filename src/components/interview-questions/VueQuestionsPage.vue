88d0a456d8571da25d5f7c42917cbf0072a5f858
Yuriy Shandor <yuriysutosoft@gmail.com>
2022-07-20 13:14:39 +0300

    add vue interview questions

--- src/components/interview-questions/VueQuestionsPage.vue
+++ src/components/interview-questions/VueQuestionsPage.vue
@@ -50,7 +50,7 @@
             </p>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="04. What are some of the main features of Vue.js?"
+        <el-collapse-item name="04. What are the advantages of using Vue.js?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
@@ -154,8 +154,8 @@
             <p>
               Vue <b>Single File Components</b> (aka <b>*.vue</b> files, abbreviated as SFC)
               is a special file format that allows us to encapsulate the template
-              (<code>&lt;template></code>), logic (<code>&lt;script></code>),
-            and styling (<code>&lt;style></code>) of a Vue component in a single file.
+              (<code class="code">&lt;template></code>), logic (<code class="code">&lt;script></code>),
+            and styling (<code class="code">&lt;style></code>) of a Vue component in a single file.
             </p>
           </div>
         </el-collapse-item>
@@ -203,81 +203,206 @@
             </p>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="10. What are watchers?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              10. What are watchers?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <p>
+              Watchers are a more generic way to react to data changes
+              instead of using computed properties.
+            </p>
+            <p>
+              They should be used when asynchronous or expensive operations
+              need to be executed in response to changing data.
+            </p>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="11. What is the difference between one-way data flow or one-way data binding and two-way data binding?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              10. What is the difference between one-way data flow or
+              one-way data binding and two-way data binding?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <p>
+              In one-way data binding or one-way data flow, the view (UI) part
+              of the application does not update automatically. In this model,
+              when the data Model is changed, you need to write some custom code
+              to make it updated every time after the change. The <b>v-bind directive</b>
+              is used for one-way data flow or binding in Vue.js
+            </p>
+            <p>
+              On the other hand, in two-way data binding, the view (UI) part of the
+              application is automatically updated when the data Model is changed.
+              The <b>v-model directive</b> is used for two way data binding in Vue.js.
+            </p>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="12. What is the difference between registering a component locally and globally?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              12. What is the difference between registering a component locally and globally?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <p>
+              If a component is registered globally it can be used
+              in the template of any component instance within this application:
+            </p>
+            <div class="code-example">
+              <pre>
+                <code class="language-javascript">
+                  const app = Vue.createApp({});
+
+                  app.component('component-a', {
+                    /* ... */
+                  });
+
+                  app.mount('#app');
+                </code>
+              </pre>
+            </div>
+            <p>
+              Global registration can unnecessarily increase your JavaScript bundle
+              if you are using build systems like Webpack. If you stop using
+              a component in your code, it will still be included in the final build.
+            </p>
+            <p>
+              To avoid this, we can register components locally by defining them in
+              the component where it is needed:
+            </p>
+            <div class="code-example">
+              <pre>
+                <code class="language-javascript">
+                  import ComponentA from './ComponentA.vue';
+
+                  export default {
+                    components: {
+                      ComponentA,
+                    },
+                    // ...
+                  };
+                </code>
+              </pre>
+            </div>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="13. What is a virtual DOM?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              13. What is a virtual DOM?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <p>
+              In Vue, the Document Object Model is represented by the Virtual DOM,
+              which is a JavaScript object (DOM). Instead of updating the DOM directly,
+              the application updates the Virtual DOM. As a result, it reduces
+              the real DOM's updating cost, which is computationally expensive.
+              The timing at which the Virtual DOM is rendered can be controlled
+              with Virtual DOM. Until you pick it, Virtual DOM will just keep the
+              data in its current state without re-rendering it. Virtual DOM can
+              also help you improve the performance of your web apps by reducing
+              the number of times the DOM needs to be updated.
+            </p>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="14. What is reactivity in Vue.js?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              14. What is reactivity in Vue.js?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <p>
+              Reactivity – When you change the data value, the page is updated
+              to reflect the changes. Data properties and computed properties
+              in Vue are reactive. Vue.js services are not reactive.
+              Visit Reactivity in Vue.js for additional information.
+            </p>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="15. Describe the lifecycle hooks in Vue.js?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              15. Describe the lifecycle hooks in Vue.js?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <p>
+              Lifecycle hooks are functions that every Vue instance runs through.
+              Each vue has eight lifecycle hooks.
+            </p>
+            <ul>
+              <li>
+                <b>Before Create</b> - This is the first lifecycle hook
+                that is called when a Vue instance has been created.
+              </li>
+              <li>
+                <b>Created</b> - It's called immediately after the 'Before creates' hook,
+                but the Vue instance has already set initial properties, data, etc.
+              </li>
+              <li>
+                <b>Before mount</b> - invoked right before the instance is mounted on the DOM.
+                The template has been completed at this time.
+              </li>
+              <li>
+                <b>Mounted</b> - This is the name given to the template once
+                it has been filled with data and is fully functional.
+              </li>
+              <li>
+                <b>Before Updated</b> - When any changes to data require the DOM to be changed,
+                this method is called before the update.
+              </li>
+              <li>
+                <b>Updated</b> - It's called after the DOM has been updated.
+              </li>
+              <li>
+                <b>Before destroy</b> - It's a location where you can clean up your
+                resources before terminating the Vue instance.
+              </li>
+              <li>
+                <b>Destroyed</b> - When all Vue instances have been destroyed,
+                this method is called. When you call the destruct method
+                on an object in code, it will be activated.
+              </li>
+            </ul>
           </div>
         </el-collapse-item>
-        <el-collapse-item name="Example Title"
+        <el-collapse-item name="16. What are some of the most important directives in Vue?"
           class="interview-questions-accordion__item">
           <template #title>
             <div class="interview-questions-accordion__item-title">
-              Example Title
+              16. What are some of the most important directives in Vue?
             </div>
           </template>
           <div class="interview-questions-accordion__item-content">
-            Example Content
+            <ul>
+              <li>
+                <b>v-if</b> adds or removes DOM elements based on the given expression.
+              </li>
+              <li>
+                <b>v-else</b> displays content only when the expression
+                adjacent v-if resolves to false.
+              </li>
+              <li>
+                <b>v-show</b> is similar to v-if, but it renders all elements
+                to the DOM and then uses the CSS display property to show/hide elements.
+              </li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
           </div>
         </el-collapse-item>
         <el-collapse-item name="Example Title"
