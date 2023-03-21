716d060b3192aac0f8a67f62282aabfea06794c1
YuriyShandor <yuriyshandor@gmail.com>
2023-03-21 18:13:11 +0200

    add swiper examples

--- src/components/external-libraries/swiper/OneActiveSlide.vue
+++ src/components/external-libraries/swiper/OneActiveSlide.vue
@@ -4,7 +4,7 @@
       Simple Slider
     </div>
     <div class="code-example__description">
-      Simple slider with one active slide. dgfdfgdfgdf
+      Simple slider with one active slide.
     </div>
     <a
       href="https://github.com/YuriyShandor/code-examples/blob/vue3-with-typescript/src/components/ui-elements/modals/ModalWithoutPlugins.vue"
@@ -14,9 +14,15 @@
       <img src="/images/github-logo.svg" alt="" class="code-example__button-image">
     </a>
     <div class="code-example__block">
-      <div class="swiper-slider-wrap">
+      <div class="swiper-slider-wrap simple-slider">
         <swiper
+          :modules="modules"
           :slides-per-view="1"
+          :navigation="true"
+          :pagination="{
+            clickable: true,
+          }"
+          :speed="750"
           @swiper="onSwiper"
           @slideChange="onSlideChange">
           <swiper-slide
@@ -34,16 +40,17 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts">/* eslint-disable import/extensions */
 import { defineComponent, onMounted, reactive } from 'vue';
-// Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination } from 'swiper';
 import { PixabayImageObject } from '@/types';
 import PixabayApiHelper from '@/api-helpers/pixabay.api-helper';
 
 // Import Swiper styles
-// eslint-disable-next-line import/extensions
 import 'swiper/scss';
+import 'swiper/scss/pagination';
+import 'swiper/css/navigation';
 
 export default defineComponent({
   name: 'OneActiveSlide',
@@ -74,6 +81,7 @@ export default defineComponent({
     });
 
     return {
+      modules: [Navigation, Pagination],
       state,
       onSwiper,
       onSlideChange,
@@ -83,137 +91,67 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.open-modal-button {
-  max-width: 340px;
-  margin-bottom: 15px;
+.swiper-slider-wrap {
+  width: 100%;
+  margin-top: 20px;
+  border-radius: 10px;
+  overflow: hidden;
 
   @media only screen and (min-width: 700px) {
-    margin-bottom: 20px;
+    margin-top: 25px;
+    border-radius: 15px;
   }
 
   @media only screen and (min-width: 1200px) {
-    margin-bottom: 25px;
+    margin-top: 30px;
+    border-radius: 20px;
   }
 }
 
-.modal-block {
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-}
+.swiper-slide-content {
+  width: 100%;
+  height: 300px;
 
-.modal-wrap {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1001;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  opacity: 0;
-  pointer-events: none;
-  -webkit-tap-highlight-color: transparent;
-  transition: all, .5s;
+  @media only screen and (min-width: 700px) {
+    height: 450px;
+  }
 
-  &.visible {
-    opacity: 1;
-    pointer-events: auto;
-    transition: all, .01s;
-
-    .modal {
-      transform: translateX(0);
-    }
+  @media only screen and (min-width: 1200px) {
+    height: 700px;
   }
 }
 
-.modal-overlay {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  opacity: 1;
-  background-color: rgba(0, 0, 0, .7);
-  backdrop-filter: blur(17px);
-}
-
-.modal {
-  width: calc(100% - 20px);
-  max-width: 500px;
-  height: 90vh;
-  max-height: 600px;
-  margin: 0 auto;
-  position: relative;
-  z-index: 1;
-  background: #FFFFFF;
-  border-radius: 20px;
-  overflow: hidden;
-  transform: translateX(100vw);
-  -webkit-tap-highlight-color: transparent;
-  transition: all, .5s;
-}
-
-.modal-close-button {
-  position: absolute;
-  z-index: 10;
-  top: 15px;
-  right: 15px;
-  cursor: pointer;
-  width: 30px;
-  height: 30px;
-  background: #F7F4F0;
-  border-radius: 7px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  -webkit-tap-highlight-color: transparent;
-  transition: all, .25s;
-
-  svg {
-    display: block;
-    width: 16px;
-    height: auto;
-
-    path {
-      stroke: #553F3F;
-      transition: all, .25s;
-    }
-  }
-
-  &:hover {
-    background: darken(#F7F4F0, 7%);
-
-    svg path {
-      stroke: #FF7A74;
-    }
-  }
-}
-
-.modal-content {
+.swiper-slide-image {
   width: 100%;
   height: 100%;
-  overflow-x: hidden;
-  overflow-y: auto;
-
-  &::-webkit-scrollbar {
-    display: block;
-    width: 4px;
-    background-color: #DFDFDF;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    display: block;
-    background: #212121;
-    border-radius: 7px;
-    cursor: pointer;
-  }
+  object-fit: cover;
+  cursor: pointer;
 }
+</style>
 
-.modal-content__image {
-  display: block;
-  width: 100%;
-  height: auto;
+<style lang="scss">
+.swiper-slider-wrap.simple-slider {
+  .swiper-pagination {
+    width: auto;
+    max-width: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 10px;
+
+    .swiper-pagination-bullet {
+      width: 13px;
+      height: 13px;
+      margin: 0 7px;
+      background: rgba(0, 0, 0, 0.95);
+      border: 2px solid rgba(255, 255, 255, 0.95);
+      transition: all, .75s;
+      -webkit-tap-highlight-color: transparent;
+
+      &-active {
+        background: $dark-red;
+        border: 2px solid $dark-red;
+      }
+    }
+  }
 }
 </style>
