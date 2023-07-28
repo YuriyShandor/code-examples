2848624c02ca80836be07c37a11635131229eced
YuriyShandor <yuriyshandor@gmail.com>
2023-07-28 16:41:23 +0300

    add switcher examples

--- src/components/ui-elements/forms/veulidate-validation/InputPassword.vue
+++ src/components/ui-elements/forms/veulidate-validation/InputPassword.vue
@@ -1,32 +1,29 @@
 <template>
-  <div
-    class="input-block"
-    :class="{'error': v$.password.$error,
-      'valid': !v$.password.$error && v$.password.$dirty && v$.password.$model.length > 0}">
+  <div class="input-block" :class="{
+    'error': v$.password.$error,
+    'valid': !v$.password.$error && v$.password.$dirty && v$.password.$model.length > 0
+  }">
     <div v-if="label.length > 0" class="input-label">
       {{ label }}
     </div>
     <span class="input-wrap">
       <label :for="id">
-        <input
-          :type="(autocompleteEnabled || v$.password.$dirty) && !state.isPasswordVisible ? 'password' : 'text'"
-          :id="id"
-          class="input password"
-          v-model="v$.password.$model">
+        <input :type="(autocompleteEnabled || v$.password.$dirty) && !state.isPasswordVisible ? 'password' : 'text'"
+          :id="id" class="input password" v-model="v$.password.$model">
       </label>
-    <div
-      class="password-eye-icon"
-      :class="{'crossed-out': state.isPasswordVisible}"
-      @click="state.isPasswordVisible = !state.isPasswordVisible">
-      <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20.2565 6.962C20.7305 7.582 20.7305 8.419 20.2565 9.038C18.7635 10.987 15.1815 15 10.9995 15C6.81752 15 3.23552 10.987 1.74252 9.038C1.51191 8.74113 1.38672 8.37592 1.38672 8C1.38672 7.62408 1.51191 7.25887 1.74252 6.962C3.23552 5.013 6.81752 1 10.9995 1C15.1815 1 18.7635 5.013 20.2565 6.962V6.962Z" stroke="#C3C3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M11 11C12.6569 11 14 9.65685 14 8C14 6.34315 12.6569 5 11 5C9.34315 5 8 6.34315 8 8C8 9.65685 9.34315 11 11 11Z" stroke="#C3C3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    </div>
+      <div class="password-eye-icon" :class="{ 'crossed-out': state.isPasswordVisible }"
+        @click="state.isPasswordVisible = !state.isPasswordVisible">
+        <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M20.2565 6.962C20.7305 7.582 20.7305 8.419 20.2565 9.038C18.7635 10.987 15.1815 15 10.9995 15C6.81752 15 3.23552 10.987 1.74252 9.038C1.51191 8.74113 1.38672 8.37592 1.38672 8C1.38672 7.62408 1.51191 7.25887 1.74252 6.962C3.23552 5.013 6.81752 1 10.9995 1C15.1815 1 18.7635 5.013 20.2565 6.962V6.962Z"
+            stroke="#C3C3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path
+            d="M11 11C12.6569 11 14 9.65685 14 8C14 6.34315 12.6569 5 11 5C9.34315 5 8 6.34315 8 8C8 9.65685 9.34315 11 11 11Z"
+            stroke="#C3C3C3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </div>
     </span>
-    <div
-      v-if="v$.password.$error"
-      class="input-error">
+    <div v-if="v$.password.$error" class="input-error">
       <span v-for="error in v$.password.$errors" :key="error.$message">
         {{ error.$message }}
       </span>
